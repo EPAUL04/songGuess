a86d48e71ce35c962e39077bf8a7f5e644e11ce6
Elizabeth Paul <eandepaul@gmail.com>
2026-08-06 14:09:48 -0700

    trying to compare actual track name rather than submission itself

--- js/script.js
+++ js/script.js
@@ -307,8 +307,14 @@ function validate(guess) {
   getSongFeatures(guess);
 
   // validate it girl
-  alert("submitting " + guess); //TODO: remove
-  return songGlobal.name == guess;
+  //TODO: make this a query thing instead of just comparison
+  let token = localStorage.getItem("access_token");
+  // turn songName into actual track object
+  const findSong = await fetch(`https://api.spotify.com/v1/search?q=track%3A${songName}`, {
+    headers: { Authorization: `Bearer ${token}` }
+  });
+  alert("submitting " + findSong.json().items[0].name);
+  return songGlobal.name == findSong.json().items[0].name;
 }
 
 function win() {
