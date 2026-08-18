89e0ba8a3e03717676c1aef17ce351b5b10596d5
Elizabeth Paul <eandepaul@gmail.com>
2026-08-18 13:10:32 -0700

    testing

--- js/script.js
+++ js/script.js
@@ -336,7 +336,7 @@ async function validate(guess) {
   const found = await findSong.json();
   // console.log(found);
   // alert("submitting " + found.tracks.items[0].name);
-  let bool = (songGlobal == found.tracks.items[0]);
+  let bool = (songGlobal.name == found.tracks.items[0].name);
   alert("comparing " + found.tracks.items[0].name + " and " + songGlobal.name + " and getting " + bool);
   return songGlobal.name == found.tracks.items[0].name;
 }
