e6e5f9eb69cad9cbeaa8cd3d822c3bb989f46c0d
Elizabeth Paul <eandepaul@gmail.com>
2026-09-05 15:07:53 -0700

    testing blank submission

--- js/script.js
+++ js/script.js
@@ -256,8 +256,7 @@ async function submit(num) {
     let input = document.getElementById("answer" + num).value;
     // if submission was blank, just say false, otherwise validate submission
     let b = false;
-    if (input.value == "" || input == null) {
-      alert("claiming that submission not blank, validating");
+    if (input.value != "" || input != null) {
       b = await validate(input);
     }
     alert("b is " + b.value);
